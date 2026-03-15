fa79912f0475574e8dfde069686cd9f254e9cefb
tousef-refuge <yoyo.alsh2007@gmail.com>
2026-03-15 14:51:36 +0300

    Added FPS lock

--- src/main/java/game/core/Game.java
+++ src/main/java/game/core/Game.java
@@ -1,13 +1,33 @@
 package game.core;
 
+import static game.core.GlobalConsts.FPS;
+
 public class Game {
+    Window window;
+    final double frameTime = 1.0 / (double) FPS;
+
+    @SuppressWarnings("BusyWait")
     public Game(String title) {
-        Window window = new Window(title);
+        window = new Window(title);
 
         while (!window.shouldClose()) {
+            double startTime = time();
+            double endTime = time();
+
+            double elapsed = endTime - startTime;
+            if (elapsed < frameTime) {
+                try {
+                    Thread.sleep((long)((frameTime - elapsed) * 1000.0));
+                } catch (InterruptedException ignored) {}
+            }
+
             window.update();
             window.clear();
         }
         window.destroy();
+    }
+
+    private double time() {
+        return System.nanoTime() / 1_000_000_000.0;
     }
 }
--- src/main/java/game/core/GlobalConsts.java
+++ src/main/java/game/core/GlobalConsts.java
@@ -3,4 +3,5 @@ package game.core;
 public final class GlobalConsts {
     //initial window dimensions
     public static final float WIDTH = 1200f, HEIGHT = 675f;
+    public static final int FPS = 72;
 }
