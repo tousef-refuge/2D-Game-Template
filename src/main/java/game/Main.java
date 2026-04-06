8d7c17b78497f231704c12cc6f93d4fcbaf088a4
tousef-refuge <yoyo.alsh2007@gmail.com>
2026-04-06 05:36:46 +0300

    Moved title declaration from Main to GlobalConsts

--- src/main/java/game/Main.java
+++ src/main/java/game/Main.java
@@ -2,8 +2,10 @@ package game;
 
 import game.core.Game;
 
+import static game.core.GlobalConsts.TITLE;
+
 public class Main {
     public static void main(String[] args) {
-        new Game("2D Game Template");
+        new Game(TITLE);
     }
 }
--- src/main/java/game/core/GlobalConsts.java
+++ src/main/java/game/core/GlobalConsts.java
@@ -1,6 +1,7 @@
 package game.core;
 
 public final class GlobalConsts {
-    //initial window dimensions
+    //initial game variables
+    public static final String TITLE = "2D Game Template";
     public static final int FPS = 72;
 }
