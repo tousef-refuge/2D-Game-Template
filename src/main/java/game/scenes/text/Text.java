786f59bac204378cca9ab9e728d39bf627d67563
tousef-refuge <yoyo.alsh2007@gmail.com>
2026-04-06 12:33:32 +0300

    Added text rendering

--- src/main/java/game/scenes/text/Text.java
+++ src/main/java/game/scenes/text/Text.java
@@ -1,12 +1,15 @@
 package game.scenes.text;
 
 import game.exceptions.AssetNotFoundException;
+import game.window.Alignment;
 import org.lwjgl.BufferUtils;
+import org.lwjgl.stb.STBTTAlignedQuad;
 import org.lwjgl.stb.STBTTBakedChar;
 import org.lwjgl.stb.STBTTFontinfo;
 
 import java.io.*;
 import java.nio.ByteBuffer;
+import java.nio.FloatBuffer;
 import java.nio.IntBuffer;
 import java.util.HashMap;
 import java.util.Map;
@@ -18,20 +21,94 @@ public class Text {
     private static final int BITMAP_SIZE = 1 << 10;
     private static final Map<String, Font> fontCache = new HashMap<>();
 
-    private static Font getFont(String fontPath, float size) {
-        String key = fontPath + '&' + size;
+    public static void render(String text, String fontName,
+                              float fontSize, float borderSize,
+                              float x, float y,
+                              float r, float g, float b,
+                              float rb, float gb, float bb,
+                              float a, String align) {
+        var coords = Alignment.get(align).getCoords();
+        float nx = x + coords.x(), ny = y + coords.y();
+
+        Font font = getFont(fontName, fontSize);
+        if (borderSize != 0) {
+            glColor4f(rb, gb, bb, a);
+            drawBorder(text, font, nx, ny, borderSize);
+        }
+
+        glColor4f(r, g, b, a);
+        drawText(text, font, nx, ny);
+        glColor4f(1f, 1f, 1f, 1f);
+    }
+
+    private static Font getFont(String name, float size) {
+        String key = name + '&' + size;
 
         Font entry = fontCache.get(key);
         if (entry != null) return entry;
 
+        String path = "fonts/" + name + ".ttf";
         try {
-            ByteBuffer buffer = loadInternalResource(fontPath);
+            ByteBuffer buffer = loadInternalResource(path);
             entry = bakeFont(buffer, size);
             fontCache.put(key, entry);
             return entry;
         } catch (Exception e) {
-            throw new AssetNotFoundException("font", fontPath);
+            throw new AssetNotFoundException("font", path);
         }
+    }
+
+    private static void drawText(String text, Font font, float x, float y) {
+        glBindTexture(GL_TEXTURE_2D, font.texture());
+
+        FloatBuffer xPos = BufferUtils.createFloatBuffer(1).put(0, x);
+        FloatBuffer yPos = BufferUtils.createFloatBuffer(1).put(0, y);
+
+        for (int i = 0; i < text.length(); i++) {
+            char c = text.charAt(i);
+
+            if (c == '\n') {
+                xPos.put(0, x);
+                yPos.put(0, yPos.get(0) + font.lineHeight());
+                continue;
+            }
+            if (isInvalidChar(c)) continue;
+
+            STBTTAlignedQuad q = STBTTAlignedQuad.malloc();
+            stbtt_GetBakedQuad(font.chars(), BITMAP_SIZE, BITMAP_SIZE, mapChar(c), xPos, yPos, q, true);
+
+            glBegin(GL_QUADS);
+
+            glTexCoord2f(q.s0(), q.t0());
+            glVertex2f(q.x0(), q.y0());
+
+            glTexCoord2f(q.s1(), q.t0());
+            glVertex2f(q.x1(), q.y0());
+
+            glTexCoord2f(q.s1(), q.t1());
+            glVertex2f(q.x1(), q.y1());
+
+            glTexCoord2f(q.s0(), q.t1());
+            glVertex2f(q.x0(), q.y1());
+
+            glEnd();
+            q.free();
+        }
+    }
+
+    private static void drawBorder(String text, Font font, float x, float y, float borderSize) {
+        float[][] offsets = {
+                {borderSize, borderSize},
+                {borderSize, -borderSize},
+                {-borderSize, -borderSize},
+                {-borderSize, borderSize},
+                {borderSize, 0},
+                {-borderSize, 0},
+                {0, borderSize},
+                {0, -borderSize}
+        };
+
+        for (float[] o : offsets) drawText(text, font, x + o[0], y + o[1]);
     }
 
     private static ByteBuffer loadInternalResource(String path) throws IOException {
